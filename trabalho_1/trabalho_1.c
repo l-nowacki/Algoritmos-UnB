9c453884ec4abaccf87144d65c4a64d322d07907
nowacki <luasantilli@gmail.com>
2019-05-03 11:59:00 -0300

    Added functions

--- trabalho_1/trabalho_1.c
+++ trabalho_1/trabalho_1.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void menu_de_entrada();
 void texto_inicial();
-
-int escolher_disciplina();
-int escolher_alinhamento(int disciplina);
-int escolher_perfil();
-int escolher_meta(int alinhamento);
-int escolher_bar(int perfil);
-int escolher_idade(int disciplina);
-int escolha_invalida(int escolha);
 
 struct personagem {
   int disciplina, alinhamento, perfil, meta, bar, idade;
@@ -18,14 +12,38 @@ struct personagem {
 
 struct personagem criar_personagem();
 
+//usados para a criação do personagem
+int escolher_disciplina();
+int escolher_alinhamento(int disciplina);
+int escolher_perfil();
+int escolher_meta(int alinhamento);
+int escolher_bar(int perfil);
+int escolher_idade(int disciplina);
+int escolha_invalida(int escolha);
+
+
 void iniciar_jogo(struct personagem p);
 void display_personagem(struct personagem p);
 
+int evento_1(struct personagem p);
+
+int jokenpo();
+int melhor_de_3();
+int resultado_partida();
+
 int main()
 {
+  struct personagem teste;
+  teste.disciplina = 1;
+  teste.alinhamento = 2;
+  teste.perfil = 1;
+  teste.meta = 1;
+  teste.bar = 1;
+  teste.idade = 1;
   texto_inicial();
-  menu_de_entrada();
+  //iniciar_jogo(teste);
 
+  menu_de_entrada();
   return 0;
 }
 
@@ -200,25 +218,10 @@ int escolher_idade(int disciplina) {
 }
 
 struct personagem criar_personagem() {
+  
   struct personagem p1;
 
-  p1.disciplina = escolher_disciplina();
-
-  p1.alinhamento = escolher_alinhamento(p1.disciplina);
-
-  p1.perfil = escolher_perfil();
-
-  p1.meta = escolher_meta(p1.alinhamento);
-
-  p1.bar = escolher_bar(p1.perfil);
-
-  p1.idade = escolher_idade(p1.disciplina);
-
-  printf("\nEscreva a sua história prévia. Max de 400 characteres\n");
-  scanf(" %[^\n]s", p1.historico);
-
-  
-    /**Restrições:
+      /**Restrições:
   1. Um aluno da disciplina de medicina não pode estar nos 20s
   porque ele passou mais de dez anos no cursinho
   2. Um personagem de alinhamento bom não pode ter como meta inundar
@@ -230,18 +233,147 @@ struct personagem criar_personagem() {
   5. Se o aluno escolher a disciplina de direito ele não pode escolher o
   alinhamento bom
     **/
+
+  p1.disciplina = escolher_disciplina();
+  p1.alinhamento = escolher_alinhamento(p1.disciplina);
+  p1.perfil = escolher_perfil();
+  p1.meta = escolher_meta(p1.alinhamento);
+  p1.bar = escolher_bar(p1.perfil);
+  p1.idade = escolher_idade(p1.disciplina);
+
+  printf("\nEscreva a sua história prévia. Max de 400 characteres\n");
+  scanf(" %[^\n]s", p1.historico);
+  
   return p1;
 }
 
 void display_personagem(struct personagem p) {
+  
   printf("\nA disciplina escolhida é: %d\n", p.disciplina);
   printf("O alinhamento escolhido é: %d\n", p.alinhamento);
   printf("O perfil escolhido é: %d\n", p.perfil);
   printf("A meta escolhida é: %d\n", p.meta);
   printf("O bar escolhido é o: %d\n", p.bar);
   printf("A idade escolhida é: %d\n", p.idade);
+  
 }
 
 void iniciar_jogo(struct personagem p) {
+
+  int escolha_1, escolha_2, escolha_3, escolha_4, escolha_5;
+  
   display_personagem(p);
+
+  escolha_1 = evento_1(p);
+  
+
+}
+
+int evento_1(struct personagem p) {
+
+  int choice;
+  
+  printf("\nA chuva continua forte.\n");
+  printf("Você decide:\n");
+
+  do
+    {
+      printf("\n1 - Esperar a chuva passar\n");
+      printf("2 - Sair correndo da sala\n");
+      printf("3 - Ligar para um amigo e pedir ajuda\n");
+
+      scanf("%d", &choice);
+
+    } while (escolha_invalida(choice));
+
+  if (choice == 1) {
+    printf("\nA chuva não passou. Um raio te faz cair da cadeira. Você sai da sala. \n");
+  } else if (choice == 2) {
+    printf("\nVocê sai correndo da sala.\n");
+  } else if (choice == 3) {
+    printf("\nVocê tenta ligar para o seu amigo. Mas você lembra que não pagou a conta telefônica nesse mês. Então você sai correndo da sala.\n");
+  }
+
+  return choice;
+  
+}
+
+
+
+int jokenpo() {
+
+  int jgr, maq;
+
+  printf("\nEscolha:\n");
+  printf("1 - pedra\n");
+  printf("2 - papel\n");
+  printf("3 - tesoura\n");
+
+  scanf("%d", &jgr);
+
+  srand( time(NULL) );
+  maq = 1 + (rand() % 3);
+
+  if (maq == 1) {
+    printf("\nMaquina escolheu pedra\n");
+  } else if (maq == 2) {
+    printf("\nMaquina escolheu papel\n");
+  } else if (maq == 3) {
+    printf("\nMaquina escolheu tesoura\n");
+  }
+
+  if (jgr == maq) {
+    printf("\nEmpate\n");
+    return 0;
+  } else if (jgr == 1 && maq == 3 || jgr == 2 && maq == 1 || jgr == 3 && maq == 2) {
+    printf("\nJogador Ganhou\n");
+    return 1;
+  } else {
+    printf("\nMáquina ganhou\n");
+    return -1;
+  }
+
+}
+
+
+// se melhor_de_3() retornar positivo significa que o jogador ganhou
+// se melhor_de_3() retornar negativo significa que o jogador perdeu
+// se melhor_de_3() retornar 0, significa empate. Invocar melhor_de_3() novamente.
+int melhor_de_3() {
+  int total = 0, flag = 1, partidas = 0;
+
+  while (flag) {
+    total += jokenpo();
+    partidas++;
+
+    if (abs(total) == 2) {
+      flag = 0;
+    } else if (partidas == 3) {
+      flag = 0;
+    }
+  }
+
+  return total;
+
+}
+
+int resultado_partida() {
+  
+  int resultado;
+  
+  do
+    {
+      resultado = melhor_de_3();
+
+      if (resultado > 0) {
+	printf("\nJogador ganhou a melhor de três\n");
+	return 1;
+      } else if (resultado < 0) {
+	printf("\nMaquina ganhou a melhor de três\n");
+	return 0;
+      } else {
+	printf("\nEmpate na melhor de três\n");
+      }	
+    } while (resultado == 0);
+    
 }
